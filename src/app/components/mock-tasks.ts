9a85658da244eb04d41114aa8dd571b8fc18479b
Santiago Zarza <santiagoz@outlook.com>
2022-04-06 14:02:03 -0400

    Agregar lista de tareas e interface

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text:"Terminar primer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Hacer las compras para la cena",
+        day: 'Agosto 5 a las 15:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Investigar cosas",
+        day: 'Agosto 5 a las 18:00',
+        reminder: false
+    },
+    {
+        id:4,
+        text:"Hacer la tara",
+        day: 'Agosto 5 a las 19:00',
+        reminder: false
+    },
+]
